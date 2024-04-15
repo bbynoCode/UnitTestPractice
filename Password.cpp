cf901fc111724c1610e2a61732f43ef6b920e5a8
D <bbynocode>
2024-04-15 14:52:16 -0700

    added tests

--- Password.cpp
+++ Password.cpp
@@ -1,5 +1,6 @@
 #include "Password.h"
 #include <string>
+#include <cctype>
 
 using std::string;
 
@@ -12,16 +13,32 @@ using std::string;
 int Password::count_leading_characters(string phrase){
   int repetition = 0;
   int index = 0;
+  if(phrase.length() == 0){
+    return repetition;
+  }
+  repetition = 1;
   while( index < phrase.length()){
-    if (phrase[index] == phrase[index+1]){
+    if (phrase[index] == phrase[index+1])
       repetition++;
-      index++;
-    }
-    
+    index++;
   }
   return repetition;
 }
 
+//The function takes a string as input and determines if there are lowercase and uppercase characters in it.
 bool Password::has_mixed_case(string phrase){
- return false;
+  if(phrase.length() == 0)
+    return false;
+
+  if(phrase.length() == 1)
+    return false;
+
+  int index = 0;
+  while( index < phrase.length()){
+    if(islower(phrase[index]) && isupper(phrase[index+1])) {
+      return true;
+    }
+    index++;
+  }
+  return false;
 }
